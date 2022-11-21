096e37773759f16bad2475a69def8ae666e32181
Sarthak Mittal <200050129@iitb.ac.in>
2022-11-21 19:16:58 +0530

    Detect and align working

--- align/align.py
+++ align/align.py
@@ -1,10 +1,11 @@
 # https://sefiks.com/2020/02/23/face-alignment-for-face-recognition-in-python-within-opencv/
 
-# To align the detected faces
+# To detect and align the faces
 
 import cv2
 import math
 import numpy as np
+from matplotlib import pyplot as plt
 from PIL import Image
 
 # https://raw.githubusercontent.com/kipr/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
@@ -15,7 +16,7 @@ eye_detector = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 ## we can detect face here or use the image of faces from above code block
 # reading image
-img = cv2.imread("../pf.png")
+img = cv2.imread("../angelina.webp")
 img_raw = img.copy()
 faces = face_detector.detectMultiScale(img, 1.3, 5)
 face_x, face_y, face_w, face_h = faces[0]
@@ -25,7 +26,6 @@ cv2.imwrite('face.png',img)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 eyes = eye_detector.detectMultiScale(img_gray)
-print(eyes)
  
 index = 0
 for (eye_x, eye_y, eye_w, eye_h) in eyes:
@@ -54,7 +54,7 @@ cv2.circle(img, right_eye_center, 2, (255, 0, 0) , 2)
 cv2.line(img,right_eye_center, left_eye_center,(67,67,67),2)
 
 # rotating the image on the basis of the angle of the eyeline
-if left_eye_y < right_eye_y:
+if left_eye_y > right_eye_y:
     point_3rd = (right_eye_x, left_eye_y)
     direction = -1 #rotate same direction to clock
     print("rotate to clock direction")
@@ -92,3 +92,11 @@ if direction == -1:
 
 new_img = Image.fromarray(img_raw)
 new_img = np.array(new_img.rotate(direction * angle))
+
+faces = face_detector.detectMultiScale(new_img)
+face_x, face_y, face_w, face_h = faces[0]
+ 
+img = new_img[int(face_y):int(face_y+face_h), int(face_x):int(face_x+face_w)]
+cv2.imwrite('aligned.png',img)
+plt.imshow(img)
+plt.show()
